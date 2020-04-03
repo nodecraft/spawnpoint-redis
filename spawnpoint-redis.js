d9fd9e5434fddda5fd7b886aaefb7441848e6727
James Ross <james@jross.me>
2020-04-03 22:58:38 +0100

    chore: bump ioredis to 4.x.x
    chore: release major 2.0.0 version

--- spawnpoint-redis.js
+++ spawnpoint-redis.js
@@ -1,23 +1,24 @@
 'use strict';
 const redis = require('ioredis');
+const { ReplyError } = require('redis-errors');
 
 module.exports = require('spawnpoint').registerPlugin({
 	dir: __dirname,
 	name: "Redis",
 	namespace: "redis",
 	errors: {
-		'redis.reply_error': require('ioredis/lib/reply_error.js')
+		'redis.reply_error': ReplyError
 	},
 	exports: function(app){
 		app.redis = new redis(app.config.redis);
 		app.redis.on('error', function(err){
-			app.error('Redis error occured').debug(err);
+			app.error('Redis error occurred').debug(err);
 		});
 		app.redis.once('ready', function(){
-			app.info('Connected to redis server.');
+			app.info('Connected to Redis server.');
 		});
 		app.redis.on('reconnecting', function(){
-			app.warn('Attempting to reconnect to redis server.');
+			app.warn('Attempting to reconnect to Redis server.');
 		});
 	}
 });
